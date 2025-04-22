cde0bb4c6bc8fd68227043740fe4d1c843234fa3
NSPRAVEENKUMAR <praveenkumarn205@gmail.com>
2025-04-22 09:17:49 +0530

    ..

---
+++ frontend/src/component/Login.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function Login() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>jcdh1</h1>
+    </>
+  )
+}
+
+export default Login
